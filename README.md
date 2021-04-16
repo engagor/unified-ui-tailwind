3f51b167a98119b70757027a3c3ff4aa7e7d99f0
Thomas Van Kerckvoorde <thomas.vankerckvoorde@clarabridge.com>
2021-04-16 14:57:25 +0200

    ✨ Initial commit on Tailwind UI
    - As part of a proof of concept
    
    Co-authored-by: Cedric (Ancient One) <cedric.dhooge@engagor.com>

---
+++ README.md
@@ -0,0 +1,35 @@
+# Clarabridge Engage  🚧
+
+Clarabridge Engage Unified UI Tailwind is an implementation of our Unified UI configuration.
+
+https://github.com/ClarabridgeInc/unified-ui/blob/master/src/configuration.js
+
+## Installation
+
+Clarabridge Engage Unified UI Tailwind can be installed as an [npm package](https://www.npmjs.com/package/@clarabridge/xxxxx):
+
+```bash
+$ npm i @clarabridge/unified-ui-tailwind
+```
+
+## Basic usage
+
+`import @clarabridge/unified-ui-tailwind`
+
+## Contributing
+
+1. Do your changes on a feature branch
+2. Make a PR for your commit
+3. Get a coffee and wait for reviews
+
+## How to make a release
+
+1. Pull and make sure you got the most recent code
+2. Make a release with `release-major`, `release-minor`, `release-patch`.
+
+This will update version numbers, create a Github release and create a new version of the NPM package.
+
+## License
+````
+See license at LICENSE.txt
+
---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+const config = require('@clarabridge/unified-ui/src/configuration');
+
+module.exports = {
+    corePlugins: [
+        'backgroundColor',
+        'opacity',
+        'padding',
+        'margin',
+        'position',
+        'flex',
+        'flexDirection',
+        'flexGrow',
+        'flexShrink',
+        'flexWrap',
+        'alignItems',
+        'alignContent',
+        'justifyContent',
+        'justifyItems',
+        'justifySelf',
+        'alignSelf',
+        'display',
+        'maxWidth',
+        'borderStyle',
+        'borderWidth',
+        'borderColor',
+        'borderRadius',
+        'cursor',
+        'float',
+        'clear',
+        'textAlign',
+        'textTransform',
+        'verticalAlign',
+        'wordBreak',
+        'whitespace',
+        'textOverflow',
+        'textDecoration',
+        'textColor',
+        'fontFamily',
+        'fontSize',
+        'lineHeight',
+        'transitionProperty',
+    ],
+    purge: [],
+    important: true,
+    darkMode: false,
+    theme: {
+        screens: false,
+        colors: config.COLORS,
+        spacing: config.SPACING.reduce((accumulator, spacing) => (accumulator[spacing] = spacing + 'px', accumulator), {}),
+        maxWidth: {
+            ...config.WIDTHS_AND_HEIGHTS.PERCENT.reduce((accumulator, dimension) =>
+                (accumulator[dimension+'-percent'] = dimension + '%', accumulator), {}),
+            ...config.WIDTHS_AND_HEIGHTS.PIXEL.reduce((accumulator, dimension) =>
+                (accumulator[dimension] = dimension + 'px', accumulator), {}),
+        },
+        borderWidth: config.BORDERS.reduce((accumulator, border) =>
+            (accumulator[border] = border + 'px', accumulator), {}),
+        borderRadius: config.BORDERS.reduce((accumulator, border) =>
+            (accumulator[border] = border + 'px', accumulator), {}),
+        fontFamily: config.FONT.FAMILIES,
+        fontSize: config.FONT.SIZES,
+        lineHeight: config.FONT.SIZES,
+    },
+    variants: {
+        backgroundColor: [
+            'hover'
+        ],
+        opacity: [
+            'hover'
+        ],
+        padding: [],
+        margin: [],
+        maxWidth: [],
+        borderColor: [],
+    },
+    plugins: [],
+}
